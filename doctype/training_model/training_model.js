13492574e74f8c8ffa8c02e1d404e44709823833
Shruti Gupta <85094153+Iconic-Techie@users.noreply.github.com>
2025-01-20 19:57:44 -0500

    Update training_model.js

--- doctype/training_model/training_model.js
+++ doctype/training_model/training_model.js
@@ -7,21 +7,51 @@
 // 	},
 // });
 
-
-//On Training Model Form- if we click button train -   refresh form and add new custom button with name Training-training_data field- execute the function
 frappe.ui.form.on("Training Model", {
     refresh: function(frm) {
+
+        console.log(frm.doc.training_label);
+
         frm.add_custom_button(__('Training'), function() {
-            console.log(frm.doc.training_data);
-            console.log(frm.doc.training_label);
+
+
+            //API CALL on click on Training button inside Train
+            frappe.call({
+                //change path for method accordingly
+                method:"frappe.api.api.getTestData",
+                args: {
+                     training_label: frm.doc.training_label,
+                     training_data: frm.doc.training_data,
+                 },
+                callback: function(response) {
+                    if(response.exc) {
+                        console.log("Error",response.exc);
+                    } else {
+                        console.log(response.message);
+                    }
+                }
+            })
+
         }, __("Train"));
     }
+
+    
 });
 
+// //On Training Model Form- if we click button train -   refresh form and add new custom button with name Training-training_data field- execute the function
+// frappe.ui.form.on("Training Model", {
+//     refresh: function(frm) {
+//         frm.add_custom_button(__('Training'), function() {
+//             console.log(frm.doc.training_data);
+//             console.log(frm.doc.training_label);
+//         }, __("Train"));
+//     }
+// });
 
-//On Training Model Form- if something happens in training_data field- execute the function
-frappe.ui.form.on("Training Model",'training_data', function(frm) {
-    // frappe.msgprint("Training Data Added ");
-    console.log(frm.doc.training_data);
-    console.log(frm.doc.training_label);
-   });
+
+// //On Training Model Form- if something happens in training_data field- execute the function
+// frappe.ui.form.on("Training Model",'training_data', function(frm) {
+//     // frappe.msgprint("Training Data Added ");
+//     console.log(frm.doc.training_data);
+//     console.log(frm.doc.training_label);
+//    });
